1844dc247923aadb3e85ae82001e2270ed507976
Daniel Vega <daniel.vega23@d155.org>
2021-11-23 08:09:01 -0600

    some more stuff

--- Graphics notes/GameScene.swift
+++ Graphics notes/GameScene.swift
@@ -14,6 +14,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private var spinnyNode : SKShapeNode?
     var cam = SKCameraNode()
     var deathStar: SKNode!
+    var blackBall1: SKNode!
+    var blackBall2: SKNode!
+    var blackBall3: SKNode!
     var scoreLabel: SKLabelNode!
     var score = 0
     var collide = false
@@ -22,6 +25,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
         deathStar = self.childNode(withName: "deathStar")!
+        blackBall1 = self.childNode(withName: "blackBall1")!
+        blackBall2 = self.childNode(withName: "blackBall2")!
+        blackBall3 = self.childNode(withName: "blackBall3")!
         scoreLabel = self.childNode(withName: "scoreCount") as? SKLabelNode
         physicsWorld.contactDelegate = self
         self.camera = cam
@@ -47,7 +53,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     override func update(_ currentTime: TimeInterval) {
-//        deathStar = self.childNode(withName: "deathStar")!
+        blackBall1.physicsBody?.velocity.dx = 50
+        blackBall1.physicsBody?.velocity.dy = 50
         //cam.position = deathStar.position
     }
 }
